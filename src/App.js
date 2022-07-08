2e624bb7effa60d03ba4c787af0761bd43f296a0
Efty <efty131331@gmail.com>
2022-07-08 10:52:19 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
