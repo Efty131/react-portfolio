92d9016b4b03ebe4bbb458aa399066cc3eb269e3
Efty <efty131331@gmail.com>
2022-07-23 12:05:04 +0600

    thumbnail added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ import { useTypewriter } from 'react-simple-typewriter';
 import headerImg from '../assets/img/header-img.svg';
 
 export const Banner = () => {
-
+//   Typing effect
     const { text } = useTypewriter({
       words: ['Developer', 'Programmer', 'Gamer'],
       loop: 0
@@ -23,9 +23,7 @@ export const Banner = () => {
             <Row className="align-items-center">
                 <Col xm={12} md={6} xl={7}>
                     <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>{`I'm Efty `}<span className='wrap text-primary'>
-                      {text}
-                      </span></h1>
+                    <h1>{`I'm Efty `}<span className='wrap text-primary'>{text}</span></h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro reprehenderit consectetur culpa molestias saepe incidunt suscipit, et doloribus ab temporibus quo cupiditate officiis minima delectus nisi hic modi ea iusto soluta necessitatibus ex dolor.</p>
                     <button onClick={handleClick}>Let's Connect < ArrowRightCircle size={25} /> </button>
                 </Col>
